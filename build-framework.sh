30dce9a2963aec9feab1da8df18f2589c9b77235
Reese <rgrimsle@andrew.cmu.edu>
2020-10-30 20:28:06 -0400

    debugging

--- build-framework.sh
+++ build-framework.sh
@@ -32,14 +32,13 @@ find . -type f -name ".DS_Store" -delete
 find . -type f -name "._*" -delete
 popd
 echo "Creating tarball..."
-cd $SOURCE_DIR
+cd $SOURCE_DIR/../
 pwd
 # COPYFILE_DISABLE=1 tar -czvf $TARGET_FILENAME $SOURCE_DIR
 # COPYFILE_DISABLE=1 zip $TARGET_FILENAME $SOURCE_DIR
-COPYFILE_DISABLE=1 zip ../../$TARGET_FILENAME ./*
-cd ../../
+COPYFILE_DISABLE=1 zip ../$TARGET_FILENAME ./commonsense/*
+cd ../
 pwd
-
 
 echo "Computing SHA1 digest..."
 SHA=`openssl dgst -sha1 $TARGET_FILENAME | sed 's/^.* //'`  #https://unix.stackexchange.com/questions/42797/openssl-dgst-sha1-producing-an-extraneous-stdin-prefix-and-trailing-new 
